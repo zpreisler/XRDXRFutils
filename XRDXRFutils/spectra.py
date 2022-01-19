8d085c0d2f546bd8f678dfc6dea45f8638ebf13a
Zdenek Preisler <z.preisler@gmail.com>
2022-01-19 08:57:14 +0000

    spectra

---
+++ XRDXRFutils/spectra.py
@@ -0,0 +1,52 @@
+from numpy import loadtxt,arctan,pi,arange,array
+from .utils import snip,convolve
+
+class Spectra():
+    def __init__(self):
+        pass
+
+class SpectraXRF(Spectra):
+    def __init__(self):
+        super().__init__()
+
+class SpectraXRD(Spectra):
+    def __init__(self):
+        super().__init__()
+
+    def from_array(self,x):
+        self.counts = x
+        self.channel = arange(self.counts.__len__(),dtype='int')
+        self.intensity = self.relative_intensity()
+
+        return self
+
+    def from_file(self,filename):
+        self.counts = loadtxt(filename,unpack=True,dtype='int',usecols=1)
+        self.channel = arange(self.counts.__len__(),dtype='int')
+        self.intensity = self.relative_intensity()
+
+        return self
+
+    @staticmethod
+    def fce_calibration(x,a,s,beta):
+        """
+        XRD calibration function 
+            x is a channel
+        """
+        return (arctan((x+a)/s)) * 180 / pi + beta
+
+    @property
+    def theta(self):
+        return self.fce_calibration(self.channel,*self.opt)
+
+    def theta_range(self):
+        x = array([self.channel[0],self.channel[-1]])
+        return self.fce_calibration(x,*self.opt)
+
+    def background(self,n=21,std=3,m=32):
+        x = self.counts
+        return snip(convolve(x,n=n,std=std),m=m)
+
+    def relative_intensity(self,n=21,std=3,m=32):
+        y = self.counts - self.background(n=n,std=std,m=m)
+        return y / y.max()
---
+++ XRDXRFutils/utils.py
@@ -0,0 +1,23 @@
+from numpy import minimum,fft,pad
+from scipy.signal import windows
+
+def snip(z,m):
+
+    x = z.copy()
+    for p in range(1,m)[::-1]:
+        a1 = x[p:-p]
+        a2 = (x[:(-2 * p)] + x[(2 * p):]) * 0.5
+        x[p:-p] = minimum(a2,a1)
+
+    return x
+
+def convolve(z,n=21,std=3):
+    kernel = windows.gaussian(2 * n - 1,std)
+
+    y_pad = pad(z,(n,n),'edge')
+
+    f = fft.rfft(y_pad)
+    w = fft.rfft(kernel,y_pad.shape[-1])
+    y = fft.irfft(w * f)
+
+    return y[n * 2:] / sum(kernel)
