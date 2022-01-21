a1804df807ec9cd74ad8e354670c9ef52f06e225
Zdenek Preisler <z.preisler@gmail.com>
2022-01-21 16:45:00 +0000

    phasemap phasesearch

--- XRDXRFutils/__init__.py
+++ XRDXRFutils/__init__.py
@@ -3,3 +3,4 @@ from .database import DatabaseXRD,Phase,PhaseList
 from .spectra import SpectraXRD,SpectraXRF
 from .utils import snip,convolve
 from .gaussnewton import GaussNewton
+from .phasesearch import PhaseSearch,PhaseMap
