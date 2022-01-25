602519decef5cd58293abab928d16d63e3fc18ac
Zdenek Preisler <z.preisler@gmail.com>
2022-01-25 15:03:46 +0000

    phase search

---
+++ XRDXRFutils/phasesearch.py
@@ -0,0 +1,85 @@
+from .gaussnewton import GaussNewton
+from numpy import array
+
+class PhaseSearch(list):
+    """
+    Class to perform phase search.
+    """
+    def __init__(self,phases,spectra):
+        super().__init__([GaussNewton(phase,spectra) for phase in phases])
+        
+        self.spectra = spectra
+        self.intensity = spectra.intensity
+        
+        self.opt = self[0].opt
+        for g in self:
+            g.opt = self.opt
+        
+    def minimize_gamma(self,n=4):
+        for g in self:
+            for i in range(n):
+                g.minimize_gamma()
+                
+    def overlap_area(self):
+        return array([g.overlap_area() for g in self])
+    
+    def loss(self):
+        return array([g.loss() for g in self])
+    
+    def select(self):
+        idx = self.overlap_area().argmax()
+        selected = self[idx]
+        
+        self.idx = idx
+        self.selected = selected
+        
+        return selected
+    
+    def theta_correction(self,n=4):
+        for i in range(n):
+            self.select().calibration()
+            
+    def search(self):
+        
+        self.minimize_gamma()
+
+        self.select()
+        self.theta_correction()
+        self.minimize_gamma()
+        
+        return self
+    
+class PhaseMap(list):
+    """
+    Class to process images
+    """      
+    def from_data(self,data,phases):
+    
+        phases.get_theta(max_theta=53,min_intensity=0.05)
+        arr = data.data.reshape(-1,1280)
+
+        spectras = self.gen_spectras(arr)        
+        for spectra in spectras:
+            spectra.opt = [-1186.6, 1960.3, 51]
+        
+        self += [PhaseSearch(phases,spectra) for spectra in spectras]
+        
+        return self
+    
+    @staticmethod
+    def f_spectra(x):
+        return SpectraXRD().from_array(x)
+
+    def gen_spectras(self,a):
+        with Pool() as p:
+            spectras = p.map(self.f_spectra,a)
+        return spectras
+    
+    @staticmethod
+    def f_search(x):
+        return x.search()
+    
+    def search(self):
+        with Pool() as p:
+            result = p.map(self.f_search,self)
+        return PhaseMap(result)
