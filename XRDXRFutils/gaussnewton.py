0b1f2ee1db31158249e9731ad87e368184896c90
DarioZappala <dario.a.zappala@gmail.com>
2022-03-07 16:04:37 +0100

    vectorial Gauss-Newton

--- XRDXRFutils/gaussnewton.py
+++ XRDXRFutils/gaussnewton.py
@@ -1,18 +1,18 @@
 from .spectra import SpectraXRD
 
-from numpy import exp,log,pi,array,ones,zeros,full,trapz,minimum,fabs,sign,sqrt
-from numpy.linalg import pinv,inv
+from numpy import exp, log, pi, array, ones, zeros, full, trapz, minimum, fabs, sign, sqrt, square, average, clip, newaxis, concatenate
+from numpy.linalg import pinv, inv
 
 from matplotlib.pyplot import plot
 
 class GaussNewton(SpectraXRD):
     """
-    Class to calculate Gauss-Newton minimization of the synthetic and the experimental spectra.
+    Class to calculate Gauss-Newton minimization of the synthetic and the experimental spectrum.
     """
-    def __init__(self, phase, spectra, min_theta = 0, max_theta = 53, min_intensity = 0.05):
+    def __init__(self, phase, spectrum, min_theta = 0, max_theta = 53, min_intensity = 0.05):
         """
         phase: tabulated phase; Phase or PhaseList class
-        spectra: experimental spectra; Spectra class
+        spectrum: experimental spectrum; Spectra class
         """
         super().__init__()
 
@@ -23,14 +23,14 @@ class GaussNewton(SpectraXRD):
         self.min_intensity = min_intensity
 
         self.phase = phase
-        self.spectra = spectra
+        self.spectrum = spectrum
 
         """
-        Spectra
+        Spectrum
         """
-        self.opt = spectra.opt.copy()
-        self.channel = spectra.channel
-        self.intensity = spectra.intensity
+        self.opt = spectrum.opt.copy()
+        self.channel = spectrum.channel
+        self.intensity = spectrum.intensity
 
         """
         Phases
@@ -41,80 +41,198 @@ class GaussNewton(SpectraXRD):
         self.mu, self.I = self.get_theta(min_theta = min_theta, max_theta = max_theta, min_intensity = min_intensity)
 
         """
-        parameters sigma^2, gamma
+        parameters tau, g --> sigma^2, gamma
         """
-        self.sigma2 = full(len(self.mu),0.04)
-        self.gamma = ones(len(self.I))
+        #self.tau = full(len(self.mu), -6.21) # needed to have sigma2 = 0.04
+        self.tau = full(len(self.mu), 0.04) # needed to have sigma2 = 0.04
+        self.g = ones(len(self.I)) * 0.75 # needed to have gamma = 1
 
+
+    """
+    Utility functions
+    """
     def get_theta(self,**kwargs):
         return self.phase.get_theta(**kwargs)
 
-    def plot_spectra(self,*args,**kwargs):
+
+    def z(self):
+        """
+        Synthetic spectrum.
+        """
+        self.prepare_dimensional_data()
+        return self.component_full.sum(axis = 1)
+
+    def z0(self):
+        """
+        Synthetic spectrum without the rescalings of peaks.
+        """
+        self.prepare_dimensional_data()
+        return (self.I_dim * self.component_core).sum(axis = 1)
+
+
+    """
+    Redefined variables
+    """
+    @staticmethod
+    def w(x):
+        return 0.5 * (sqrt(x**2 + 1) + x)
+
+    @staticmethod
+    def der_w(x):
+        return 0.5 * (x / sqrt(x**2 + 1) + 1)
+
+    @property
+    def gamma(self):
+        return self.w(self.g)
+
+
+    # @staticmethod
+    # def u(x):
+    #     return GaussNewton.w(100 * x) / 100
+
+    # @staticmethod
+    # def der_u(x):
+    #     return GaussNewton.der_w(100 * x)
+
+    @staticmethod
+    def u(x):
+        return x
+
+    @staticmethod
+    def der_u(x):
+        return 1
+
+    @property
+    def sigma2(self):
+        return self.u(self.tau)
+
+
+    """
+    Calculations for fit
+    """
+    def prepare_dimensional_data(self):
+        # Variables along the channels
+        self.channel_dim = self.channel[:, newaxis]
+        self.theta_dim = self.theta[:, newaxis]
+
+        # Variables along the diffraction lines
+        self.mu_dim = self.mu[newaxis, :]
+        self.I_dim = self.I[newaxis, :]
+        self.g_dim = self.g[newaxis, :]
+        self.gamma_dim = self.gamma[newaxis, :]
+        self.tau_dim = self.tau[newaxis, :]
+        self.sigma2_dim = self.sigma2[newaxis, :]
+
+        # Components of the synthetic function (they go along both axes)
+        self.component_core = exp((self.theta_dim - self.mu_dim)**2 / (-2 * self.sigma2_dim))
+        self.component_full = self.I_dim * self.gamma_dim * self.component_core
+
+
+    def der_f_a_s_beta(self):
+        der_theta_a = (180 / pi) * self.opt[1] / ((self.channel_dim + self.opt[0])**2 + self.opt[1]**2)
+        der_theta_s = (-180 / pi) * (self.channel_dim + self.opt[0]) / ((self.channel_dim + self.opt[0])**2 + self.opt[1]**2)
+        aux = (self.component_full * (self.theta_dim - self.mu_dim) / self.sigma2_dim).sum(axis = 1, keepdims = True)
+        der_f_a = - der_theta_a * aux
+        der_f_s = - der_theta_s * aux
+        der_f_beta = - aux
+        return der_f_a, der_f_s, der_f_beta
+
+
+    def der_f_g(self):
+        return self.I_dim * self.component_core * self.der_w(self.g)
+
+
+    def der_f_tau(self):
+        f_dim = self.component_full.sum(axis = 1, keepdims = True)
+        return f_dim * ((self.theta_dim - self.mu_dim)**2 / (2 * (self.sigma2_dim)**2)) * self.der_u(self.tau)
+
+
+    def evolution_of_parameters(self):
+        y = self.intensity
+        f = self.component_full.sum(axis = 1)
+        r = y - f
+        return (pinv(self.Jacobian_f) @ r) # or scipy.linalg.pinv
+
+
+    def fit_GN_a_s_beta(self, alpha = 1):
+        """
+        Optimizes on a, s, beta. Keeps gamma and sigma fixed.
+        """
+        self.prepare_dimensional_data()
+
+        # Derivatives with respect to a, s, beta
+        der_f_a, der_f_s, der_f_beta = self.der_f_a_s_beta()
+
+        # Jacobian
+        self.Jacobian_f = concatenate((der_f_a, der_f_s, der_f_beta), axis = 1)
+
+        # Evolution of parameters
+        d_params = alpha * self.evolution_of_parameters()
+        self.opt += d_params
+
+
+    def fit_GN_a_s_gamma(self, alpha = 1):
+        """
+        Optimizes on a, s, gamma. Keeps beta and sigma fixed.
+        """
+        self.prepare_dimensional_data()
+
+        # Derivatives with respect to a, s
+        der_f_a, der_f_s, der_f_beta = self.der_f_a_s_beta()
+
+        # Jacobian
+        self.Jacobian_f = concatenate((der_f_a, der_f_s, self.der_f_g()), axis = 1)
+
+        # Evolution of parameters
+        d_params = alpha * self.evolution_of_parameters()
+        self.opt[:2] += d_params[:2]
+        self.g += d_params[2:]
+
+
+    def fit_GN_gamma(self, alpha = 1):
+        """
+        Optimizes on gamma. Keeps all the other parameters fixed.
+        """
+        self.prepare_dimensional_data()
+
+        # Jacobian
+        self.Jacobian_f = self.der_f_g()
+
+        # Evolution of parameters
+        d_params = alpha * self.evolution_of_parameters()
+        self.g += d_params
+
+
+    def fit_GN_sigma(self, alpha = 1):
+        """
+        Optimizes on sigma. Keeps all the other parameters fixed.
+        """
+        self.prepare_dimensional_data()
+
+        # Jacobian
+        self.Jacobian_f = self.der_f_tau()
+
+        # Evolution of parameters
+        d_params = alpha * self.evolution_of_parameters()
+        self.tau += d_params
+
+
+    """
+    Evaluation of the results
+    """
+    def plot_spectrum(self,*args,**kwargs):
         super().plot(*args,**kwargs)
 
     def plot(self,*args,**kwargs):
         plot(self.theta,self.z(),*args,**kwargs)
 
-    """
-    Derivatives
-    """
-    @staticmethod
-    def core(x,mu,sigma2):
-        return exp(-0.5 * (x - mu)**2 / sigma2)
-
-    @staticmethod
-    def dsigma2(x,mu,sigma2):
-        return 0.5 * (x - mu)**2 / sigma2**2
-
-    @staticmethod
-    def da(channel,x,a,s,mu,sigma2):
-        return -1.0 / sigma2 * 180 / pi * s / ((a + channel)**2 + s**2) * (x - mu)
-    
-    @staticmethod
-    def ds(channel,x,a,s,mu,sigma2):
-        return 1.0 / sigma2 * 180 / pi * (a + channel) / ((a + channel)**2 + s**2) * (x - mu)
-    
-    @staticmethod
-    def dbeta(x,mu,sigma2):
-        return -1.0 / sigma2 * (x - mu)
-
-    @staticmethod
-    def fgamma(gamma,delta=1.0):
-        return 0.5 * (sqrt(1 + gamma**2)) + 0.5 * gamma
-
-    @staticmethod
-    def dgamma(gamma,delta=1.0):
-        return 0.5 * (gamma / sqrt(1 + gamma**2)) + 0.5
-
-    def z(self):     
-        """
-        Synthetic spectra.
-        """
-        x = self.theta
-        y = zeros(len(x))
-        for mu,I,sigma2,gamma in zip(self.mu, self.I,
-                                     self.sigma2, self.gamma):
-            c = self.core(x,mu,sigma2)
-            y += self.fgamma(gamma) * I * c
-            
-        return y
-
-    def z0(self):     
-        """
-        Synthetic spectra.
-        """
-        x = self.theta
-        y = zeros(len(x))
-        for mu,I,sigma2,gamma in zip(self.mu, self.I,
-                                     self.sigma2, self.gamma):
-            c = self.core(x,mu,sigma2)
-            y += I * c
-            
-        return y
 
     def loss(self):
-        y = self.intensity
-        return sum((y - self.z())**2)
+        return sum(square(self.intensity - self.z()))
+
+    def fit_error(self):
+        return sqrt(average(square(self.intensity - self.z())))
+
 
     def area_fit(self):
         return trapz(self.z())
@@ -122,123 +240,32 @@ class GaussNewton(SpectraXRD):
     def area_0(self):
         return trapz(self.z0())
 
-    def overlap_0(self):
-        return trapz(minimum(self.z(),self.z0()))
 
     def overlap(self):
-        m =  minimum(self.z(),self.intensity)
+        m =  minimum(self.z(), self.intensity)
         m[m < 0] = 0
         return m
+
+    def overlap_0(self):
+        return trapz(minimum(self.z(), self.z0()))
 
     def overlap_area(self):
         return trapz(self.overlap())
 
-    def minimize_gamma(self,alpha = 1):
-        """
-        Minimize gamma
-        """
-        x = self.theta
-        y = self.intensity
-        z = zeros(len(x))
+
+    def overlap_ratio(self):
+        integral_intersection = clip(self.z(), 0, self.intensity).sum()
+        integral_data = self.intensity.sum()
+        return (integral_intersection / integral_data)
+
+
+    def component_ratio(self):
+        gamma_adjusted = self.gamma**(-sign(self.gamma - 1))
+        mask = ((self.mu >= self.theta.min()) & (self.mu <= self.theta.max()))
         
-        dgamma = []
-        for mu,I,sigma2,gamma in zip(self.mu,self.I,
-                                     self.sigma2,self.gamma):
-            c = self.core(x,mu,sigma2)
-
-            dgamma += [I * c * self.dgamma(gamma)]
-            z += self.fgamma(gamma) * I * c
+        #penalty = (self.I[mask] * gamma_adjusted[mask]).sum() / self.I[mask].sum()
+        penalty = exp( (self.I[mask] * log(gamma_adjusted[mask])).sum() / self.I[mask].sum() )
         
-        dz = y - z
-        J = array(dgamma).T
-        dr = pinv(J) @ dz
-   
-        self.gamma[:] += dr * alpha
-
-    def minimize_sigma(self,alpha = 1):
-        """
-        Minimize sigma2
-        """
-        x = self.theta
-        y = self.intensity
-        z = zeros(len(x))
-        
-        dsigma2 = []
-        for mu,I,sigma2,gamma in zip(self.mu,self.I,
-                                     self.sigma2,self.gamma):
-            c = self.core(x,mu,sigma2)
-            h = self.fgamma(gamma) * I * c
-            dsigma2 += [h * self.dsigma2(x,mu,sigma2)]
-            z += h
-        
-        dz = y - z
-        J = array(dsigma2).T
-        dr = pinv(J) @ dz
-   
-        self.sigma2[:] += dr * alpha
-
-    def autocalibrate(self,alpha = 1):
-        """
-        Autocalibration
-        """
-
-        x = self.theta
-        y = self.intensity
-
-        da = zeros(len(x))
-        ds = zeros(len(x))
-        dbeta = zeros(len(x))
-
-        z = zeros(len(x))
-        
-        for mu,I,sigma2,gamma in zip(self.mu,self.I,
-                                     self.sigma2,self.gamma):
-            c = self.core(x,mu,sigma2)
-
-            h = self.fgamma(gamma) * I * c
-            
-            da += h * self.da(self.channel,x,self.opt[0],self.opt[1],mu,sigma2)
-            ds += h * self.ds(self.channel,x,self.opt[0],self.opt[1],mu,sigma2)
-            dbeta += h * self.dbeta(x,mu,sigma2)
-
-            z += h
-        
-        dz = y - z
-        J = array([da,ds,dbeta]).T
-        
-        dr = pinv(J) @ dz
-        self.opt[:] += dr * alpha
-
-    def calibrate(self,alpha = 1):
-        """
-        Calibrate a,s and intensities
-            assuming fixed beta, and sigma
-        """
-        x = self.theta
-        y = self.intensity
-
-        da = zeros(len(x))
-        ds = zeros(len(x))
-        dbeta = zeros(len(x))
-
-        z = zeros(len(x))
-        
-        dgamma = []
-        for mu,I,sigma2,gamma in zip(self.mu,self.I,
-                                     self.sigma2,self.gamma):
-            c = self.core(x,mu,sigma2)
-            h = self.fgamma(gamma) * I * c
-
-            dgamma += [I * c * self.dgamma(gamma)]
-
-            da += h * self.da(self.channel,x,self.opt[0],self.opt[1],mu,sigma2)
-            ds += h * self.ds(self.channel,x,self.opt[0],self.opt[1],mu,sigma2)
-
-            z += h
-        
-        dz = y - z
-        J = array([da,ds] + dgamma).T
-        dr = pinv(J) @ dz
-   
-        self.opt[:2] += dr[:2] * alpha
-        self.gamma[:] += dr[2:] * alpha
+        intersection_integral = clip(self.z(), 0, self.intensity).sum()
+        data_integral = self.intensity.sum()
+        return penalty * (intersection_integral / data_integral)
