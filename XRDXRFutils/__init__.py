9d23cd41607493044639e102b7d3fb7ed5420a3d
Zdenek Preisler <z.preisler@gmail.com>
2022-04-01 10:17:56 +0000

    init PhaseMapSave

--- XRDXRFutils/__init__.py
+++ XRDXRFutils/__init__.py
@@ -3,5 +3,5 @@ from .database import DatabaseXRD,Phase,PhaseList
 from .spectra import SpectraXRD,SpectraXRF,SyntheticSpectraXRF
 from .utils import snip, convolve, snip2d, convolve2d, snip3d, convolve3d
 from .gaussnewton import GaussNewton
-from .phasesearch import PhaseSearch,PhaseMap
+from .phasesearch import PhaseSearch,PhaseMap,PhaseMapSave
 from .calibration import Calibration
