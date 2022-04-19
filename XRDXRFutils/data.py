8597f1d200d06ec6d870194d358cfe549a1e13a8
Zdenek Preisler <z.preisler@gmail.com>
2022-04-19 12:08:45 +0000

    SyntheticDataXRF fixes

--- XRDXRFutils/data.py
+++ XRDXRFutils/data.py
@@ -378,16 +378,19 @@ class SyntheticDataXRF(Data):
     """
     Namespace
     """
-    name = 'sxrf'
+    name = 'synth_xrf'
     
     def __init__(self, rl_atnum_list = None, skip_element = False):
         super().__init__()
+
         self.nbins = None
         self.rl_atnum_list = rl_atnum_list
+
         if self.rl_atnum_list:
             for i,item in enumerate(rl_atnum_list):
                 if not isinstance(item, int):
                     raise TypeError(f'{item} at index {i} is not integer.\nIntegers are expected for Atomic Numbers')
+
         self.skip_element = skip_element
     
     def __len__(self):
@@ -401,17 +404,22 @@ class SyntheticDataXRF(Data):
         self.nbins = nbins
     
     def read(self, outdata_path):
+
         if not self.rl_atnum_list:
             raise ValueError("Atomic numbers list required to read the data\nSet 'rl_atnum_list' attribute or initialize a new instance.")
+
         self.rl_atnum_list = sorted(self.rl_atnum_list)
         self.path = outdata_path
         xmso_filenames = []
+
         if not os.path.isdir(outdata_path):
             raise FileNotFoundError(f"No such file or directory: {outdata_path}")
+
         for path, dirs, files in os.walk(outdata_path):
             # to do: use glob to select xmso files
             for _file in files:
                 xmso_filenames.append(os.path.join(path, _file))
+
         print(f"Reading SXRF data from {outdata_path}")
         self.metadata["rl_atnum_list"] = self.rl_atnum_list
         self.spe_objs = [s for s in self.__read__(xmso_filenames) if s != None]
@@ -463,44 +471,55 @@ class SyntheticDataXRF(Data):
         return results
     
     def get_sim_parameters(self, local = False):
+
         if not hasattr(self, 'spe_objs'):
             raise RuntimeError("xmso files not readed yet")
         len_data = len(self.spe_objs)
+
         if local:
             self.time = empty((len_data))
             self.weight_fractions = zeros((len_data,len(self.rl_atnum_list)))
-            self.reflayer_thicknes = empty((len_data))
-            self.sublayer_thicknes = empty((len_data))
+            self.reflayer_thickness = empty((len_data))
+            self.sublayer_thickness = empty((len_data))
+
             for i, s in enumerate(self.spe_objs):
                 self.time[i] = s.time
                 self.weight_fractions[i] = s.weight_fractions
-                self.reflayer_thicknes[i] = s.reflayer_thicknes
-                self.sublayer_thicknes[i] = s.sublayer_thicknes
+                self.reflayer_thickness[i] = s.reflayer_thickness
+                self.sublayer_thickness[i] = s.sublayer_thickness
             return
         sp = SimParameters(len_data)
+
         for i, s in enumerate(self.spe_objs):
             sp.time[i] = s.time
             # sp.reflayer_elements += s.reflayer_elements
             sp.weight_fractions.append(s.weight_fractions)
-            sp.reflayer_thicknes[i] = s.reflayer_thicknes
-            sp.sublayer_thicknes[i] = s.sublayer_thicknes
+            sp.reflayer_thickness[i] = s.reflayer_thickness
+            sp.sublayer_thickness[i] = s.sublayer_thickness
         sp.weight_fractions = asarray(sp.weight_fractions).reshape(len_data, -1)
+
         return sp
         
     
     def save_h5(self, filename = None):
+
         if not hasattr(self, 'spe_objs'):
-            raise RuntimeError("xmso files not readed yet")
+            raise RuntimeError("xmso files not read yet")
+
         if not hasattr(self, "data"):
             raise RuntimeError("Data and labels not yet genarated")
+
         if filename == None:
             if hasattr(self, "path"):
                 filename = os.path.join(self.path, self.name + '.h5')
             else:
                 filename = os.path.join(os.getcwd(), self.name + '.h5')
-        if not hasattr(self,'reflayer_thicknes'):
+
+        if not hasattr(self,'reflayer_thickness'):
             self.get_sim_parameters(local = True)
+
         self.metadata["rl_atnum_list"] = self.rl_atnum_list
+
         print('Saving:',filename)
         with h5py.File(filename,'w') as f:
 
@@ -509,8 +528,8 @@ class SyntheticDataXRF(Data):
 
             dataset = f.create_dataset('data', data = self.data)
             dataset = f.create_dataset('labels', data = self.labels)
-            dataset = f.create_dataset('reflayer_thicknes', data = self.reflayer_thicknes)
-            dataset = f.create_dataset('sublayer_thicknes', data = self.sublayer_thicknes)
+            dataset = f.create_dataset('reflayer_thickness', data = self.reflayer_thickness)
+            dataset = f.create_dataset('sublayer_thickness', data = self.sublayer_thickness)
             # dataset = f.create_dataset('reflayer_elements', data = sp.reflayer_elements)
             dataset = f.create_dataset('weight_fractions', data = self.weight_fractions)
             dataset = f.create_dataset('energy', data = self.energy)
@@ -523,12 +542,14 @@ class SyntheticDataXRF(Data):
             
             self.data = f['data'][:]
             self.labels = f['labels'][:]
-            self.reflayer_thicknes = f['reflayer_thicknes'][:]
-            self.sublayer_thicknes = f['sublayer_thicknes'][:]
+            self.reflayer_thickness = f['reflayer_thickness'][:]
+            self.sublayer_thickness = f['sublayer_thickness'][:]
             # self.reflayer_elements = f['reflayer_elements'][:]
             self.weight_fractions = f['weight_fractions'][:]
             self.energy = f['energy'][:]
             self.time = f['time'][:]
+
+            self._x = self.energy
 
             for k,v in f.attrs.items():
                 self.metadata[k] = v
@@ -540,11 +561,13 @@ class SimParameters:
     Synthetic data parameters class
     """
     def __init__(self, len_data = 1):
+
         self.len_data = len_data
         self.time = empty((len_data))
+
         self.weight_fractions = []
-        self.reflayer_thicknes = empty((len_data))
-        self.sublayer_thicknes = empty((len_data))
+        self.reflayer_thickness = empty((len_data))
+        self.sublayer_thickness = empty((len_data))
 
 class DataXRD(Data):
     """
