4fb4a2245ae4d10d167e52e0c5dd37581fe2b409
DarioZappala <dario.a.zappala@gmail.com>
2022-09-28 10:29:33 +0200

    remove obsolete classes

--- XRDXRFutils/__init__.py
+++ XRDXRFutils/__init__.py
@@ -3,7 +3,6 @@ from .database import DatabaseXRD, Phase, PhaseList
 from .spectra import SpectraXRD, SpectraXRF, SyntheticSpectraXRF, FastSpectraXRD
 from .utils import snip, convolve, snip2d, convolve2d, snip3d, convolve3d
 from .gaussnewton import GaussNewton
-from .phasesearch import PhaseSearch, PhaseMap, PhaseMapSave
 from .gammasearch import GammaSearch, GammaMap
 from .gammasearch_secondary import GammaSearch_Secondary, GammaMap_Secondary
 from .chisearch import ChiSearch, ChiMap
