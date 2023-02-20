8d232e33d485591c0498302d311b3a6c37dc7fc7
rosario <andolinarosario@gmail.com>
2023-02-20 17:52:08 +0100

    mendeleev.dat missing

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@
 from distutils.core import setup
 
 setup(name='XRDXRFutils',
-        version='1.0',
-        description='XRD and XRF utilities',
-        author='Zdenek Preisler',
-        author_email='z.preisler@gmail.com',
-        packages=['XRDXRFutils'],
-        )
+    version='0.21',
+    description='XRD and XRF utilities',
+    author='Zdenek Preisler',
+    author_email='z.preisler@gmail.com',
+    packages=['XRDXRFutils'],
+    package_dir={'XRDXRFutils' : 'XRDXRFutils'},
+    package_data={'XRDXRFutils' : ['mendeleev.dat']}
+    )
