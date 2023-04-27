511e3fcb6c3175d1e54fa693db0353fa0f51c6a4
DarioZappala <dario.a.zappala@gmail.com>
2023-04-27 11:18:39 +0200

    class GaussNewton_2Phases

--- XRDXRFutils/gaussnewton.py
+++ XRDXRFutils/gaussnewton.py
@@ -458,3 +458,260 @@ class GaussNewton(FastSpectraXRD):
             return pl_new[0]
         else:
             return pl_new
+
+
+
+class GaussNewton_2Phases(GaussNewton):
+    """Class to calculate Gauss-Newton minimization of the synthetic and the experimental spectrum."""
+
+    def __init__(self, phase1, phase2, spectrum, sigma = 0.2, **kwargs):
+        """
+        Initialization of GaussNewton
+        - phase1, phase2: (Phase or PhaseList class)
+            Tabulated phase.
+        - spectrum: (SpectraXRD class)
+            Experimental spectrum.
+        - sigma: (float)
+            Standard deviation of Gaussian peaks of the synthetic XRD patterns. Default is 0.2.
+        - kwargs: (different types, optional)
+            Arguments to select peaks; they are passed to Phase.get_theta().
+        """
+        for phase in [phase1, phase2]:
+            if type(phase) not in [Phase, PhaseList]:
+                raise Exception('GaussNewton initialization: invalid phase type.')
+
+        self.phases = [phase1, phase2]
+        self.spectrum = spectrum
+        self.kwargs = kwargs
+        self.label = phase1.label + ' + ' + phase2.label
+        self.opt = spectrum.opt[[0, 0, 1, 2]].copy()
+
+        ### Variables along the diffraction lines ###
+        # tabulated theta: mu
+        # tabulated intensity: I
+        # parameters g, tau --> gamma, sigma^2
+
+        self.mu, self.I = [], []
+        for idx in [0, 1]:
+            mu, I, p = self.get_theta_partial(idx)
+            self.mu.append(mu)
+            self.I.append(I)
+
+        self.g = [full(self.n_peaks[idx], self.iw(1)) for idx in [0, 1]]
+        self.tau = [full(self.n_peaks[idx], self.iu(sigma**2)) for idx in [0, 1]]
+
+
+    ### Redefined variables ###
+
+    @property
+    def gamma(self):
+        return [self.w(self.g[idx]) for idx in [0, 1]]
+
+    @property
+    def sigma2(self):
+        return [self.u(self.tau[idx]) for idx in [0, 1]]
+
+
+    ### Utility functions ###
+
+    @property
+    def n_peaks(self):
+        """Number of tabulated peaks of each phase."""
+        return [self.mu[idx].shape[0] for idx in [0, 1]]
+
+    def get_theta_partial(self, idx):
+        """Tabulated peaks of the chosen phase."""
+        return self.phases[idx].get_theta(**self.kwargs)
+
+
+    @property
+    def theta(self):
+        """Angles corresponding to channels, according to calibration of each phase."""
+        return [
+            self.spectrum.fce_calibration(
+                self.channel,
+                self.opt[idx], self.opt[2], self.opt[3]
+            ) for idx in [0, 1]
+        ]
+
+    @property
+    def theta_range(self):
+        """Angular range, according to calibration of each phase."""
+        return [
+            self.spectrum.fce_calibration(
+                array([self.channel[0], self.channel[-1]]),
+                self.opt[idx], self.opt[2], self.opt[3]
+            ) for idx in [0, 1]
+        ]
+
+
+    def synthetic_spectrum(self, idx, rescale_peaks = False):
+        mu = self.mu[idx][newaxis, :]
+        I = self.I[idx][newaxis, :]
+        sigma2 = self.sigma2[idx][newaxis, :]
+        theta = self.theta[idx][:, newaxis]
+
+        component_core = exp((theta - mu)**2 / (-2 * sigma2))
+        if (rescale_peaks):
+            gamma = self.gamma[idx][newaxis, :]
+            component_full = I * gamma * component_core
+        else:
+            component_full = I * component_core
+        return component_full.sum(axis = 1)
+
+    def z0_partial(self, idx):
+        """Synthetic spectrum of the chosen phase, with gamma = 1 for all peaks."""
+        return self.synthetic_spectrum(idx, False)
+
+    def z_partial(self, idx):
+        """Synthetic spectrum of the chosen phase."""
+        return self.synthetic_spectrum(idx, True)
+
+    def z0(self):
+        """Synthetic spectrum of the combination of phases, with gamma = 1 for all peaks."""
+        return self.z0_partial(0) + self.z0_partial(1)
+
+    def z(self):
+        """Synthetic spectrum of the combination of phases."""
+        return self.z_partial(0) + self.z_partial(1)
+
+
+    ### Calculations for fit ###
+
+    def calculate_components(self):
+        mu = [arr[newaxis, :] for arr in self.mu]
+        I = [arr[newaxis, :] for arr in self.I]
+        gamma = [arr[newaxis, :] for arr in self.gamma]
+        sigma2 = [arr[newaxis, :] for arr in self.sigma2]
+        theta = [arr[:, newaxis] for arr in self.theta]
+
+        self.component_core = [exp((theta[idx] - mu[idx])**2 / (-2 * sigma2[idx])) for idx in [0, 1]]
+        self.component_full = [I[idx] * gamma[idx] * self.component_core[idx] for idx in [0, 1]]
+
+    def del_components(self):
+        del self.component_core
+        del self.component_full
+
+
+    def der_f_a_s_beta(self):
+        mu = [arr[newaxis, :] for arr in self.mu]
+        sigma2 = [arr[newaxis, :] for arr in self.sigma2]
+        channel = self.channel[:, newaxis]
+        theta = [arr[:, newaxis] for arr in self.theta]
+
+        a = [self.opt[idx] for idx in [0, 1]]
+        s = self.opt[2]
+
+        der_denominator = [(channel + a[idx])**2 + s**2 for idx in [0, 1]]
+        der_theta_a = [rad2deg(s / der_denominator[idx]) for idx in [0, 1]]
+        der_theta_s = [rad2deg((channel + a[idx]) / der_denominator[idx]) for idx in [0, 1]]
+
+        aux = [(self.component_full[idx] * (theta[idx] - mu[idx]) / sigma2[idx]).sum(axis = 1, keepdims = True) for idx in [0, 1]]
+
+        der_f_a = [- der_theta_a[idx] * aux[idx] for idx in [0, 1]]
+        der_f_s = - der_theta_s[0] * aux[0] - der_theta_s[1] * aux[1]
+        der_f_beta = - aux[0] - aux[1]
+
+        return (der_f_a[0], der_f_a[1], der_f_s, der_f_beta)
+
+
+    def der_f_g(self):
+        I = [arr[newaxis, :] for arr in self.I]
+        return tuple(I[idx] * self.component_core[idx] * self.der_w(self.g[idx]) for idx in [0, 1])
+
+
+    def der_f_tau(self):
+        mu = [arr[newaxis, :] for arr in self.mu]
+        sigma2 = [arr[newaxis, :] for arr in self.sigma2]
+        theta = [arr[:, newaxis] for arr in self.theta]
+        return tuple(self.component_full * ((theta[idx] - mu[idx])**2 / (2 * sigma2[idx]**2)) * self.der_u(self.tau[idx]) for idx in [0, 1])
+
+
+    def fit(self, a = False, s = False, beta = False, gamma = False, sigma = False, alpha = 1, downsample = None):
+        """
+        Performs a step of Gauss-Newton optimization.
+        You need to choose the parameters that will be used to optimize. The other ones will be kept fixed.
+        """
+
+        def f(self, a, s, beta, gamma, sigma, alpha):
+            if (self.n_peaks[0] + self.n_peaks[1] > 0):
+
+                if (self.n_peaks[0] + self.n_peaks[1] == 1):
+                    s = False
+                n_opt = 2 * a + s + beta
+                n_gamma = [n_peaks * gamma for n_peaks in self.n_peaks]
+                n_sigma = [n_peaks * sigma for n_peaks in self.n_peaks]
+
+                self.calculate_components()
+
+                ### Construction of Jacobian ###
+                Jacobian_construction = []
+
+                # Calibration parameters
+                if (n_opt > 0):
+                    der_f_a1, der_f_a2, der_f_s, der_f_beta = self.der_f_a_s_beta()
+                if a:
+                    Jacobian_construction.extend((der_f_a1, der_f_a2))
+                if s:
+                    Jacobian_construction.append(der_f_s)
+                if beta:
+                    Jacobian_construction.append(der_f_beta)
+
+                # Gamma
+                if gamma:
+                    Jacobian_construction.extend(self.der_f_g())
+
+                # Sigma
+                if sigma:
+                    Jacobian_construction.extend(self.der_f_tau())
+
+                # Jacobian
+                Jacobian_f = concatenate(Jacobian_construction, axis = 1)
+
+                ### Evolution of parameters ###
+                y = self.intensity[:, newaxis]
+                f = concatenate(self.component_full, axis = 1).sum(axis = 1, keepdims = True)
+                r = y - f
+                try:
+                    evol = pinv(Jacobian_f) @ r
+                except:
+                    evol = full((Jacobian_f.shape[1], 1), 0)
+                d_params = alpha * evol
+
+                # Add evolution
+                mask_opt = [a, a, s, beta]
+                self.opt[mask_opt] += d_params[0 : n_opt, 0]
+
+                if gamma:
+                    i_current = n_opt
+                    self.g[0] += d_params[i_current : (i_current + n_gamma[0]), 0]
+                    i_current += n_gamma[0]
+                    self.g[1] += d_params[i_current : (i_current + n_gamma[1]), 0]
+
+                if sigma:
+                    i_current += n_gamma[1]
+                    self.tau[0] += d_params[i_current : (i_current + n_sigma[0]), 0]
+                    i_current += n_sigma[0]
+                    self.tau[1] += d_params[i_current : (i_current + n_sigma[1]), 0]
+
+                self.del_components()
+
+            return self
+
+        return self.downsampled_function(downsample, f, a = a, s = s, beta = beta, gamma = gamma, sigma = sigma, alpha = alpha)
+
+
+
+    ### Evaluation of the results ###
+
+    def overlap_partial(self, idx, downsample = None):
+        def f(self):
+            return maximum(minimum(self.z_partial(idx), self.intensity), 0)
+
+        return self.downsampled_function(downsample, f)
+
+
+    ### Plot functions ###
+
+    def plot_phase(self, idx, **kwargs):
+        self.phases[idx].plot(**self.kwargs, **kwargs)
